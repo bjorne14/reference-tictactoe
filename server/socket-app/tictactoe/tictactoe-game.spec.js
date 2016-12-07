3c649edaf81706cda59e7b677571d64c75b8bc9a
ingo <bjorne14@ru.is>
2016-12-07 17:03:19 +0000

    Refactoring

--- server/socket-app/tictactoe/tictactoe-game.spec.js
+++ server/socket-app/tictactoe/tictactoe-game.spec.js
@@ -7,25 +7,41 @@ var tictactoe = require('./tictactoe-handler')(inject({
     TictactoeState
 }));
 
-var createEvent = {
+ /* Global testing data for the creation and joining of games since that will be used in multiple tests.
+  * (for clarity they should ALLWAYS be cased in the same manner as the actual commands)
+  * 
+  * PlayerA will allways create the game, PlayerB joins the game successfully.
+  */
+var CreateGame = {
+    id: "1337",
+    type: "CreateGame",
+    name: "PlayerA's epic tictactoe party",
+    timeStamp: "2030-03-02T13:37:00",
+};
+
+var GameCreated = {
     type: "GameCreated",
-    user: {
-        userName: "TheGuy"
-    },
-    name: "TheFirstGame",
-    timeStamp: "2014-12-02T11:29:29"
+    name: "PlayerA's epic tictactoe party",
+    timeStamp: "2030-03-02T13:37:00",
+    side: 'X'
 };
 
-var joinEvent = {
+var JoinGame = {
+    type: "JoinGame",
+    user: { userName: "PlayerB" },
+    name: "PlayerA's epic tictactoe party",
+    timeStamp: "2030-03-02T13:37:00"        
+};
+
+var GameJoined = {
     type: "GameJoined",
-    user: {
-        userName: "Gummi"
-    },
-    name: "TheFirstGame",
-    timeStamp: "2014-12-02T11:29:29"
+    user: { userName: "PlayerB" },
+    name: "PlayerA's epic tictactoe party",
+    timeStamp: "2030-03-02T13:37:00",
+    side: 'O' 
 };
 
-
+// Unit test for the creation of TicTacToe games
 describe('create game command', function() {
 
 
@@ -45,34 +61,14 @@ describe('create game command', function() {
 
 
     it('should emit game created event', function(){
-
         given = [];
-        when =
-        {
-            id:"123987",
-            type: "CreateGame",
-            user: {
-                userName: "TheGuy"
-            },
-            name: "TheFirstGame",
-            timeStamp: "2014-12-02T11:29:29"
-        };
-        then = [
-            {
-                type: "GameCreated",
-                user: {
-                    userName: "TheGuy"
-                },
-                name: "TheFirstGame",
-                timeStamp: "2014-12-02T11:29:29",
-                side:'X'
-            }
-        ];
-
+        when = CreateGame;
+        then = [ GameCreated ];
     })
 });
 
 
+// Unit test for joining a created TicTacToe game
 describe('join game command', function () {
 
 
@@ -92,61 +88,13 @@ describe('join game command', function () {
 
 
     it('should emit game joined event...', function () {
-
-        given = [{
-            type: "GameCreated",
-            user: {
-                userName: "TheGuy"
-            },
-            name: "TheFirstGame",
-            timeStamp: "2014-12-02T11:29:29"
-        }
-        ];
-        when =
-        {
-            type: "JoinGame",
-            user: {
-                userName: "Gummi"
-            },
-            name: "TheFirstGame",
-            timeStamp: "2014-12-02T11:29:29"
-        };
-        then = [
-            {
-                type: "GameJoined",
-                user: {
-                    userName: "Gummi"
-                },
-                name: "TheFirstGame",
-                timeStamp: "2014-12-02T11:29:29",
-                side:'O'
-            }
-        ];
-
+        given = [ CreateGame, GameCreated ];
+        when = JoinGame;
+        then = [ GameJoined ];
     });
 
     it('should emit FullGameJoinAttempted event when game full', function () {
-        given = [
-            {
-                type: "GameCreated",
-                user: { userName: "PlayerA" },
-                name: "PlayerA's epic tictactoe party",
-                timeStamp: "2030-03-02T13:37:01"
-            },
-            {
-                type: "JoinGame",
-                user: { userName: "PlayerB" },
-                name: "PlayerA's epic tictactoe party",
-                timeStamp: "2030-03-02T13:37:02"
-            },
-            {
-                type: "GameJoined",
-                user: { userName: "PlayerB" },
-                name: "PlayerA's epic tictactoe party",
-                timeStamp: "2030-03-02T13:37:03",
-                side: '0'
-            }          
-        ];
+        given = [ CreateGame, GameCreated, JoinGame, GameJoined ];
         when = {
             type: "JoinGame",
             user: { userName: "PlayerC" },
