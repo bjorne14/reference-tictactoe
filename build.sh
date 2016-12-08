65823d62505ebcf9f79c8cd9ad56ff7264a9331e
ingo <bjorne14@ru.is>
2016-12-08 13:09:52 +0000

    Improved commenting on scripts

--- build.sh
+++ build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+#
+# Script which builds the project and copies necessary scripts into the build folder.
+#
 #Author: Björn Ingemar Elfström
 
-#Export NODE_PATH as root directory.
+#Export NODE_PATH as current directory.
 export NODE_PATH=.
 
-#Clean and Build project.
+#Clean, Create and Build project.
 npm run clean
 npm run createbuild
 npm run buildclient
 
-#Move Built project into build folder
+#Move Built client files into build folder
 mv client/build build/static
+
+#Copy Built server files into build folder.
 cp -R server build/server
 mkdir -p build/client/src
 cp -r client/src/common build/client/src
+
+#Copy run script into build folder.
 cp run.js build
